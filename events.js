25c73b430037919cb8162c09b82378991c7c7e91
kapi104 <skorupa7979@gmail.com>
2023-03-06 18:49:59 +0100

    remodeled display method, added display for dot, created backspace function

--- events.js
+++ events.js
@@ -1,14 +1,46 @@
 const numBtns = document.querySelectorAll('.num');
 const display = document.querySelector('.display');
+const displayNumber = document.querySelectorAll('.dnumber');
+const dot = document.querySelectorAll('.dot');
+const backspace = document.querySelector('.backspace')
 let a = 0, b = 0;
-console.log(numBtns)
+let currentNumber = 0;
+let dotCount = 0;
 
 const changeDisplay = function(num) {
-  display.textContent += num;
+  displayNumber[currentNumber].textContent = num;
+  currentNumber += 1;
+}
+
+const clearDisplay = function() {
+  displayNumber.forEach(num => {
+    num.textContent = '';
+    currentNumber = 0;
+    dotCount = 0;
+  })
+}
+
+const back = function() {
+  if (currentNumber > 0)
+  currentNumber -= 1;
+  displayNumber[currentNumber].textContent = '';
 }
 
 numBtns.forEach(btn => {
     btn.addEventListener('click', () => {
-      changeDisplay(btn.textContent);
+      if (currentNumber != 12) {
+        if (btn.textContent != '.') {
+          changeDisplay(btn.textContent);
+        } else if (currentNumber != 0 && dotCount == 0) {
+          changeDisplay(btn.textContent);
+          dotCount = 1;
+        }  
+      }
     })
   })
+
+document.querySelector('.clear').addEventListener('click', () => {
+  clearDisplay();
+})
+
+backspace.addEventListener('click', () => back())
