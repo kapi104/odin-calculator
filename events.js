1ce1c223297f0b173b41ebbc60cae6c3abfe2caf
kapi104 <skorupa7979@gmail.com>
2023-03-06 20:05:35 +0100

    linked operator buttons with functions

--- events.js
+++ events.js
@@ -2,8 +2,11 @@ const numBtns = document.querySelectorAll('.num');
 const display = document.querySelector('.display');
 const displayNumber = document.querySelectorAll('.dnumber');
 const dot = document.querySelectorAll('.dot');
-const backspace = document.querySelector('.backspace')
-let a = 0, b = 0;
+const backspace = document.querySelector('.backspace');
+const operator = document.querySelectorAll('.operator');
+const equal = document.querySelector('.operate');
+
+let a = 0, b = 0, oper;
 let currentNumber = 0;
 let dotCount = 0;
 
@@ -26,6 +29,16 @@ const back = function() {
   displayNumber[currentNumber].textContent = '';
 }
 
+const getNumber = function() {
+  const arr = Array.from(displayNumber);
+  let num = arr.map(number => number.textContent);
+  return num.join('');
+}
+
+const resetOperatorColor = function(){
+  operator.forEach(op => op.style.backgroundColor = 'orange')
+}
+
 numBtns.forEach(btn => {
     btn.addEventListener('click', () => {
       if (currentNumber != 12) {
@@ -36,6 +49,7 @@ numBtns.forEach(btn => {
           dotCount = 1;
         }  
       }
+      operator.forEach(op => op.style.backgroundColor = 'orange')
     })
   })
 
@@ -44,3 +58,24 @@ document.querySelector('.clear').addEventListener('click', () => {
 })
 
 backspace.addEventListener('click', () => back())
+
+operator.forEach(op => {
+  op.addEventListener('click', action => {
+    a = Number(getNumber());
+    console.log(a);
+    oper = action.target.attributes[1].value;
+    console.log(action.target.attributes[1].value);
+    resetOperatorColor();
+    action.target.style.backgroundColor = 'rgb(201, 130, 0)';
+    clearDisplay();
+  })
+})
+
+equal.addEventListener('click', () => {
+  b = Number(getNumber());
+  console.log(a+oper+b)
+  let result = String(operate(a, b, oper));
+  let resultArray = [...result]
+  console.log(resultArray);
+  
+})
--- operations.js
+++ operations.js
@@ -14,9 +14,19 @@ const divide = function(a, b) {
   return a / b;
 }
 
+const power = function(a, b) {
+  return Math.pow(a, b);
+}
+
+const factorial = function(a, b) {
+  return Math.factorial(a, b);
+}
+
 const operate = function(a, b, operator) {
-  if(operator == '+') return add(a,b);
-  if(operator == '-') return substract(a,b);
-  if(operator == '*') return multiply(a,b);
-  if(operator == '/') return divide(a,b);
+  if(operator == 'plus') return add(a,b);
+  if(operator == 'minus') return substract(a,b);
+  if(operator == 'multiply') return multiply(a,b);
+  if(operator == 'divide') return divide(a,b);
+  if(operator == 'power') return power(a,b);
+  if(operator == 'factorial') return factorial(a,b);
 }
